d5add7fab905da5e74536e33f8548c64c7d5e0e7
kendrinab <kendra.barrera01@gmail.com>
2020-10-24 22:31:33 -0400

    adding searchform component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,13 @@
 import React from 'react';
-//import { AppContextProvider } from './context/AppContext';
-//import ContextDemo from './components/ContextDemo';
-
-import './App.css';
 import SearchForm from './components/SearchForm';
 
+import './App.css';
+
 const App = () => {
-  const [serverMessage, setServerMessage] = useState('');
-
-  const fetchDemoData = () => {
-    fetch('/api/demo')
-      .then((response) => response.json())
-      .then((data) => setServerMessage(data.message));
-  };
-
-  useEffect(fetchDemoData, []);
-
   return (
-    <AppContextProvider>
-      <div id="demo">
-        <h3>Hello</h3>
-        <SearchForm />
-        <h3>{serverMessage}</h3>
-      </div>
-    </AppContextProvider>
+    <div>
+      <SearchForm />
+    </div>
   );
 };
 
---
+++ client/src/components/Searchform.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+
+const SearchForm = ({ handleSubmitProp }) => {
+  return (
+    <Form onSubmit={handleSubmitProp} style={{ margin: '3rem' }}>
+      <Form.Row>
+        <Form.Control
+          id="searchbar"
+          type="text"
+          size="sm"
+          placeholder="Search for a Beatles song"
+        ></Form.Control>
+      </Form.Row>
+    </Form>
+  );
+};
+
+export default SearchForm;
